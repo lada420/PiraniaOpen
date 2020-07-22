6902957b4e2736301cab646fe1b73038a3e6f133
ngololobov_dev <neptu133@gmail.com>
2020-07-22 21:57:03 +0300

    {PIR-4}[feature] Stats interface added

---
+++ Pirania_v_2/New Unity Project/Assets/Scripts/Stats.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using StateModifier_ns;
+
+
+namespace Stats_ns
+{
+
+    public class Stats 
+    {
+        public float BaseValue;
+        int coord = 4;
+    
+        public float Value {
+            get 
+            {
+                if(isDirty)
+                {
+                    _value =  CalculateValue();
+                    isDirty = false;
+                }
+                return _value;
+            }
+        }
+    
+        private bool isDirty = true;
+        private float _value;
+    
+        private readonly List<StateModifier> stateModifiers;
+    
+        public Stats(float baseValue)
+        {
+            BaseValue = baseValue;
+            stateModifiers = new List<StateModifier>();
+        }
+    
+        public void AddModifier(StateModifier mod)
+        {
+            isDirty = true;
+            stateModifiers.Add(mod);
+            stateModifiers.Sort(CompareModifierOrder);
+        }
+    
+        private int CompareModifierOrder(StateModifier lhs, StateModifier rhs)
+        {
+            if (lhs.Order < rhs.Order)
+                return -1;
+            else if(lhs.Order > rhs.Order)
+                return 1;
+            return 0;
+        }
+    
+        public bool RemoveModifier(StateModifier mod)
+        {
+            isDirty = true;
+            return stateModifiers.Remove(mod);
+        }
+    
+        private float CalculateValue()
+        {
+            float finalValue = BaseValue;
+    
+            foreach(StateModifier stat in stateModifiers)
+            {
+                StateModifier mod = stat;
+                if(mod.Type == StatModType.Flat)
+                {
+                    finalValue += stat.Value;
+                }
+                else if(mod.Type == StatModType.Percent)
+                {
+                    finalValue *= 1 + mod.Value;
+                }
+            }
+            return (float)Math.Round(finalValue, 4);
+        }
+    
+        public float GetCoordinates()
+        {
+            return coord;
+        }
+    
+    
+    }
+}
